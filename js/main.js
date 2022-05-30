398c6702915d5d54e6136a1426f8a15e261ab284
VeresSvetlana <88767400+VeresSvetlana@users.noreply.github.com>
2022-05-30 18:07:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ document.getElementById('an').innerHTML = data.getFullYear();
 
 function mobileMenu() {
     var x = document.getElementById("navbar");
-    if (x.className==="nav"){
-        x.className+= " mobile";
+    if (x.className === "nav"){
+        x.className += "nav mobile";
     } else {
         x.className = "nav";
     }
